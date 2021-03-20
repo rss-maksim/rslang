dafddbaefe7a474a777aa794b3960e52b61b4223
Maksim Kirilko <maksimkirilko@gmail.com>
2021-03-20 13:23:09 +0200

    #1: Init project and add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    root: true,
+    overrides: [
+        {
+            files: ["*.ts"],
+            parserOptions: {
+                project: ["tsconfig.*?.json", "e2e/tsconfig.json"],
+                createDefaultProgram: true,
+            },
+            extends: ["plugin:@angular-eslint/recommended"],
+            rules: {},
+        },
+        {
+            files: ["*.component.html"],
+            extends: ["plugin:@angular-eslint/template/recommended"],
+            rules: {
+                "max-len": ["error", { code: 140 }],
+            },
+        },
+        {
+            files: ["*.component.ts"],
+            extends: ["plugin:@angular-eslint/template/process-inline-templates"],
+        },
+    ],
+};
