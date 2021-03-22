bc98aebab12973e93936391bec446090797e05cf
Antonina Dubich <dubich.t@gmail.com>
2021-03-23 01:37:26 +0300

    feat: add header

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
     styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+    iconImagePath = "../../../../assets/images/icons/header/"
+    isAuthorization = true;
+    userPhoto = "https://ofigenno.com/wp-content/uploads/posts/z/zoi-saldana/1.jpg";
+    iconHome = `${this.iconImagePath}home.svg`;
+    iconTextbook = `${this.iconImagePath}textbook.svg`;
+    iconMiniGames = `${this.iconImagePath}game.svg`;
+    iconStatistic = `${this.iconImagePath}statistics.svg`;
+
+    headerMenu = [
+      {
+        name: 'Home',
+        router: ['/home'],
+        srcImg: this.iconHome
+      },
+      {
+        name: 'Textbook',
+        router: ['/dictionary'],
+        srcImg: this.iconTextbook
+      },
+      {
+        name: 'Mini Games',
+        router: ['/mini-games'],
+        srcImg: this.iconMiniGames
+      },
+      {
+        name: 'Statistics',
+        router: ['/statistics'],
+        srcImg: this.iconStatistic
+      }
+    ]
+
     constructor() {}
+
 }
