d8c772dd9feab47603a0fc52fbf49e76a8125b65
Ilya Kozak <kozak@tut.by>
2021-03-23 19:53:41 +0300

    feat: add game sprint background

--- src/app/mini-games/pages/mini-games-page/mini-games-page.component.ts
+++ src/app/mini-games/pages/mini-games-page/mini-games-page.component.ts
@@ -7,37 +7,43 @@ type GameCard = {
   description: string;
 };
 
+const GAME_CARDS: GameCard[] = [
+  {
+    name: 'Спринт',
+    icon: 'timer',
+    route: 'sprint',
+    description:
+      'Учит быстро переводить с английского на ваш родной язык. Для этой тренировки используются слова из вашего словаря',
+  },
+  {
+    name: 'Аудиовызов',
+    icon: 'audiotrack',
+    route: 'audiochallenge',
+    description: 'Улучшает восприятие английской речи на слух',
+  },
+  {
+    name: 'Саванна',
+    icon: 'pets',
+    route: 'savannah',
+    description: 'Оттачивает понимание речи и быстрого перевода слов, помогает не забыть выученные слова',
+  },
+  {
+    name: 'Своя игра',
+    icon: 'contact_support',
+    route: 'custom-game',
+    description: 'Помогает не забывать уже выученные слова, тренировка проходит по алгоритму "кривой забывания"',
+  },
+];
+
 @Component({
   selector: 'app-mini-games-page',
   templateUrl: './mini-games-page.component.html',
   styleUrls: ['./mini-games-page.component.scss'],
 })
 export class MiniGamesPageComponent {
-  gameCards: GameCard[] = [
-    {
-      name: 'Спринт',
-      icon: 'timer',
-      route: 'sprint',
-      description:
-        'Учит быстро переводить с английского на ваш родной язык. Для этой тренировки используются слова из вашего словаря',
-    },
-    {
-      name: 'Аудиовызов',
-      icon: 'audiotrack',
-      route: 'audiochallenge',
-      description: 'Улучшает восприятие английской речи на слух',
-    },
-    {
-      name: 'Саванна',
-      icon: 'pets',
-      route: 'savannah',
-      description: 'Оттачивает понимание речи и быстрого перевода слов, помогает не забыть выученные слова',
-    },
-    {
-      name: 'Своя игра',
-      icon: 'contact_support',
-      route: 'custom-game',
-      description: 'Помогает не забывать уже выученные слова, тренировка проходит по алгоритму "кривой забывания"',
-    },
-  ];
+  gameCards: GameCard[] = GAME_CARDS;
+
+  trackByCardName(index: number, card: GameCard) {
+    return card.name;
+  }
 }
