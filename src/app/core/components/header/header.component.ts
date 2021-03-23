74cd2b60281c7ac5cc4bd3cc94673efe445a54b1
Antonina Dubich <dubich.t@gmail.com>
2021-03-23 13:52:25 +0300

    feat: add trackBy

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,42 +1,51 @@
 import { Component } from '@angular/core';
 
+export interface IHeaderMenu {
+  name: string;
+  router: string[];
+  srcImg: string;
+}
+
 @Component({
-    selector: 'app-header',
-    templateUrl: './header.component.html',
-    styleUrls: ['./header.component.scss'],
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-    iconImagePath = "../../../../assets/images/icons/header/"
-    isAuthorization = true;
-    userPhoto = "https://ofigenno.com/wp-content/uploads/posts/z/zoi-saldana/1.jpg";
-    iconHome = `${this.iconImagePath}home.svg`;
-    iconTextbook = `${this.iconImagePath}textbook.svg`;
-    iconMiniGames = `${this.iconImagePath}game.svg`;
-    iconStatistic = `${this.iconImagePath}statistics.svg`;
+  iconImagePath = '../../../../assets/images/icons/header/';
+  isAuthorization = true;
+  userPhoto = 'https://ofigenno.com/wp-content/uploads/posts/z/zoi-saldana/1.jpg';
+  iconHome = `${this.iconImagePath}home.svg`;
+  iconTextbook = `${this.iconImagePath}textbook.svg`;
+  iconMiniGames = `${this.iconImagePath}game.svg`;
+  iconStatistic = `${this.iconImagePath}statistics.svg`;
 
-    headerMenu = [
-      {
-        name: 'Home',
-        router: ['/home'],
-        srcImg: this.iconHome
-      },
-      {
-        name: 'Textbook',
-        router: ['/dictionary'],
-        srcImg: this.iconTextbook
-      },
-      {
-        name: 'Mini Games',
-        router: ['/mini-games'],
-        srcImg: this.iconMiniGames
-      },
-      {
-        name: 'Statistics',
-        router: ['/statistics'],
-        srcImg: this.iconStatistic
-      }
-    ]
+  headerMenu = [
+    {
+      name: 'Home',
+      router: ['/home'],
+      srcImg: this.iconHome,
+    },
+    {
+      name: 'Textbook',
+      router: ['/dictionary'],
+      srcImg: this.iconTextbook,
+    },
+    {
+      name: 'Mini Games',
+      router: ['/mini-games'],
+      srcImg: this.iconMiniGames,
+    },
+    {
+      name: 'Statistics',
+      router: ['/statistics'],
+      srcImg: this.iconStatistic,
+    },
+  ];
 
-    constructor() {}
+  constructor() {}
 
+  identify(index: number, item: IHeaderMenu) {
+    return item.name;
+  }
 }
