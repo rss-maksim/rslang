41ad618c92e62160db910a8d04f613c70a71449e
Antonina Dubich <dubich.t@gmail.com>
2021-03-24 00:37:34 +0300

    feat: add footer

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+
+import { selectTeammates } from '../../../redux/selectors/home.selector';
+import { Teammate } from '../../../redux/models/teammate.model';
+import { AppState } from '../../../redux/models/state.model';
+import { fetchTeam } from '../../../redux/actions';
+import { RSSLinks } from '../../constants/index';
 
 @Component({
-    selector: 'app-footer',
-    templateUrl: './footer.component.html',
-    styleUrls: ['./footer.component.scss'],
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {
-    constructor() {}
+export class FooterComponent implements OnInit {
+  teammates$: Observable<Teammate[]> = this.store.select(selectTeammates);
+  rssImage = RSSLinks.image;
+  rssUrl = RSSLinks.url;
+  constructor(private store: Store<AppState>) {}
+
+  onClick() {
+    console.log(this.teammates$);
+  }
+
+  ngOnInit() {
+    this.store.dispatch(fetchTeam());
+  }
 }
