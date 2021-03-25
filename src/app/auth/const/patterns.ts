c6a5aedab8d3f8d1a1c2e5487c9e05c4339f188c
Maksim Kirilko <maksimkirilko@gmail.com>
2021-03-25 22:10:59 +0200

    #33: Update regexp

--- src/app/auth/const/patterns.ts
+++ src/app/auth/const/patterns.ts
@@ -1,1 +1,1 @@
-export const regExpEmailPattern = new RegExp(/(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}/);
+export const regExpEmailPattern = new RegExp(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*(\W)+).{8,}/g);
