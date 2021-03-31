8eae4ebbf23cedcedb14ea0c39317fc6acb90a6a
Ivan Grakov <ivan.grakov@gmail.com>
2021-03-31 20:24:23 +0300

    feat: #15 add user driven rounds start

--- src/app/mini-games/custom-mini-game/custom-mini-game.component.ts
+++ src/app/mini-games/custom-mini-game/custom-mini-game.component.ts
@@ -13,11 +13,11 @@ import { SoundService } from './services/sound.service';
 })
 export class CustomMiniGameComponent implements OnInit, OnDestroy {
   sourceArray = [
-    ['humor', 'юмор'],
-    ['height', 'высота'],
-    ['school', 'школа'],
-    ['season', 'время года'],
-    ['calendar', 'календарь'],
+    ['humor', 'юмор', 'не выучено'],
+    ['height', 'высота', 'не выучено'],
+    ['school', 'школа', 'не выучено'],
+    ['season', 'время года', 'не выучено'],
+    ['calendar', 'календарь', 'не выучено'],
   ];
   currentWord?: string;
   currentWordTranslation?: string;
@@ -28,186 +28,165 @@ export class CustomMiniGameComponent implements OnInit, OnDestroy {
   currentWordIndex = 0;
   countdownTimer = 0;
   tick = 1_000;
-  roundLength = 8; // длина раунда в тиках
-  timeoutBetweenRounds = 2_000;
+  roundLength = 2; // длина раунда в тиках
   countDown?: Subscription;
+  currentRound = this.currentWordIndex + 1;
   roundsLeft = this.sourceArray.length - 1;
   isGamePaused = false;
   isSoundOn = false;
-  isGameLost = false;
+  isRoundOver = false;
+  isGameOver = false;
   errorsCounter = 0;
-  imageSrc = '../../../assets/images/mini-games/custom-mini-game/hangman_stage' + this.errorsCounter + '.png';
+  srcCommPart = '../../../assets/images/mini-games/custom-mini-game/hangman_stage';
+  imageSrc = this.srcCommPart + this.errorsCounter + '.png';
+  gameOverSoundDelay = 2_000;
+  isResultsShown = false;
 
   constructor(public dialog: MatDialog, private soundService: SoundService, private shuffleService: ShuffleService) {}
 
   ngOnInit() {
-    this.startGame();
-  }
-
-  startGame(): void {
     this.nextRoundReset();
   }
 
-  compareWords(wordOne: string, wordTwo: string): boolean {
-    return wordOne === wordTwo;
-  }
-
   nextRoundReset(): void {
-    console.log('Entering nextRoundReset()');
-    console.log('movesCountdownCounter', this.movesCountdownCounter);
-    console.log('currentWordIndex', this.currentWordIndex);
-    console.log('roundsLeft', this.roundsLeft);
-    console.log();
-    console.log('___________');
-    console.log();
-
-    // debugger;
-
-    this.isGamePaused = false;
     this.countDown?.unsubscribe();
+    this.isGamePaused = false;
+    this.isRoundOver = false;
     this.countdownTimer = this.roundLength; // reset timer
 
-    if (!this.isGameOver()) {
-      this.currentWord = this.sourceArray[this.currentWordIndex][0];
-      this.currentWordTranslation = this.sourceArray[this.currentWordIndex][1];
-      this.scrambledWordAsArray = this.shuffleService.shuffleLettersInWord(this.currentWord);
-      this.scrambledWord = this.scrambledWordAsArray.join('');
-      this.movesCountdownCounter = this.currentWord.length; // reset moves counter
+    this.currentWord = this.sourceArray[this.currentWordIndex][0]; //TODO переделать на запрос из сервиса
+    this.currentWordTranslation = this.sourceArray[this.currentWordIndex][1]; //TODO переделать на запрос из сервиса
 
-      this.countDown = timer(0, this.tick) // start timer countdown
-        .subscribe(() => {
-          if (this.countdownTimer > 0 && !this.isGameOver()) {
-            if (!this.isGamePaused) {
-              this.countdownTimer -= 1;
-            }
+    this.scrambledWordAsArray = this.shuffleService.shuffleLettersInWord(this.currentWord);
+    this.scrambledWord = this.scrambledWordAsArray.join('');
+    this.movesCountdownCounter = this.currentWord.length; // reset moves counter
+
+    this.countDown = timer(0, this.tick) // start timer countdown
+      .subscribe(() => {
+        if (this.countdownTimer > 0) {
+          if (!this.isGamePaused) {
+            this.countdownTimer -= 1;
           }
-          if (this.countdownTimer === 0 || this.isGameOver()) {
-            this.countDown?.unsubscribe();
-            if (!this.isGameOver()) {
-              this.errorsCounter += 1;
+        }
+        if (this.countdownTimer === 0) {
+          this.isRoundOver = true;
+          this.soundService.playAudio('round lost');
+          this.errorsCounter += 1;
+          this.sourceArray[this.currentWordIndex][2] = 'неправильно';
+          this.imageSrc = this.srcCommPart + this.errorsCounter + '.png';
 
-              console.log('errorsCounter', this.errorsCounter);
-
-              this.imageSrc =
-                '../../../assets/images/mini-games/custom-mini-game/hangman_stage' + this.errorsCounter + '.png';
-              this.soundService.playAudio('round lost');
-
-              // debugger;
-              console.log('Before changing currentWordIndex and roundsLeft');
-              console.log('movesCountdownCounter', this.movesCountdownCounter);
-              console.log('currentWordIndex', this.currentWordIndex);
-              console.log('roundsLeft', this.roundsLeft);
-              console.log('this.isGameOver(): ', this.isGameOver());
-              console.log();
-              console.log('___________');
-              console.log();
-
-              //this.currentWordIndex += 1;
-              //this.roundsLeft -= 1;
-
-              console.log('After changing currentWordIndex and roundsLeft');
-              console.log('movesCountdownCounter', this.movesCountdownCounter);
-              console.log('currentWordIndex', this.currentWordIndex);
-              console.log('roundsLeft', this.roundsLeft);
-              console.log('this.isGameOver(): ', this.isGameOver());
-              console.log();
-              console.log('___________');
-              console.log();
-
-              setTimeout(() => {
-                // задерживаем начало следующего раунда
-                console.log('-----');
-                console.log('this.isGameOver(): ', this.isGameOver());
-                console.log('-----');
-                if (!this.isGameOver()) {
-                  this.nextRoundReset();
-                }
-              }, this.timeoutBetweenRounds);
-            }
+          if (this.errorsCounter === 5 || this.isLastRound()) {
+            this.isGameOver = true;
+            setTimeout(() => {
+              this.finalize();
+            }, this.gameOverSoundDelay);
+            // TODO вывод статистики за игру (функция)
           }
-          if (this.isGameOver()) {
-            if (this.errorsCounter < 5) {
-              this.soundService.playAudio('round won');
-            } else {
-              this.soundService.playAudio('round lost');
-            }
-          }
-        });
+
+          this.countDown?.unsubscribe();
+        }
+      });
+  }
+
+  isLastRound() {
+    if (this.currentWordIndex === this.sourceArray.length - 1) {
+      return true;
     }
+    return false;
+  }
+
+  startNextRound() {
+    // Начинаем следующий раунд
+    console.log('In startNextRound()');
 
     this.currentWordIndex += 1;
-    this.roundsLeft -= 1;
+    if (this.currentRound < this.sourceArray.length) {
+      this.currentRound += 1;
+    }
+    if (this.roundsLeft > 0) {
+      this.roundsLeft -= 1;
+    }
 
-    /*     if (this.isGameOver()) {
-      if (this.errorsCounter < 5) {
-        this.soundService.playAudio('round won');
-      } else {
-        this.soundService.playAudio('round lost');
-      }
-    } */
-  }
+    if (this.currentWordIndex === this.sourceArray.length) {
+      this.isGameOver = true;
+      setTimeout(() => {
+        this.finalize();
+      }, this.gameOverSoundDelay);
+      // TODO вывод статистики за игру (функция)
+    }
 
-  isRoundLost(): boolean {
-    if (this.countdownTimer === 0 || this.movesCountdownCounter === 0) return true;
-    return false;
-  }
-
-  isRoundWon(): boolean {
-    if (this.currentWord === this.scrambledWordAsArray?.join('')) return true;
-    return false;
-  }
-
-  isGameOver(): boolean {
-    if (this.currentWordIndex >= this.sourceArray.length || this.errorsCounter >= 5) return true;
-    return false;
+    if (!this.isGameOver) {
+      this.nextRoundReset();
+    }
   }
 
   dropLetter(event: CdkDragDrop<string[]>) {
     if (this.scrambledWordAsArray) {
       this.previousScrambledWord = this.scrambledWordAsArray.join('');
-      if (this.movesCountdownCounter > 0 && !this.isGameOver()) {
+      if (this.movesCountdownCounter > 0) {
         moveItemInArray(this.scrambledWordAsArray, event.previousIndex, event.currentIndex);
       }
       if (this.previousScrambledWord !== this.scrambledWordAsArray.join('')) {
         // если буква сдвинулась, то ход засчитываем
-        if (this.movesCountdownCounter > 0 && !this.isGameOver()) this.movesCountdownCounter -= 1;
+        if (this.movesCountdownCounter > 0) this.movesCountdownCounter -= 1;
         this.soundService.playAudio('move');
         this.previousScrambledWord = this.scrambledWordAsArray.join('');
       }
-      if (this.isRoundLost() && !this.isGameOver()) {
+
+      if (this.movesCountdownCounter === 0) {
         this.isGamePaused = true;
         this.soundService.playAudio('round lost');
         this.errorsCounter += 1;
-        this.imageSrc =
-          '../../../assets/images/mini-games/custom-mini-game/hangman_stage' + this.errorsCounter + '.png';
-        setTimeout(() => {
-          // задерживаем начало следующего раунда
-          this.nextRoundReset();
-        }, this.timeoutBetweenRounds);
+        this.imageSrc = this.srcCommPart + this.errorsCounter + '.png';
+        this.isRoundOver = true;
+        this.sourceArray[this.currentWordIndex][2] = 'неправильно';
+        if (this.isLastRound()) {
+          this.isGameOver = true;
+          setTimeout(() => {
+            this.finalize();
+          }, 2_000);
+          // TODO вывод статистики за игру (функция)
+        }
       }
-      if (this.isRoundWon() && !this.isGameOver()) {
+      if (this.currentWord === this.scrambledWordAsArray?.join('')) {
         this.isGamePaused = true;
         this.soundService.playAudio('round won');
-        setTimeout(() => {
-          // задерживаем начало следующего раунда
-          this.nextRoundReset();
-        }, this.timeoutBetweenRounds);
-      }
-
-      /*       if (this.isGameOver()) {
-        if (this.errorsCounter < 5) {
-          this.soundService.playAudio('round won');
-        } else {
-          this.soundService.playAudio('round lost');
+        this.isRoundOver = true;
+        this.sourceArray[this.currentWordIndex][2] = 'правильно';
+        if (this.isLastRound()) {
+          this.isGameOver = true;
+          setTimeout(() => {
+            this.finalize();
+          }, this.gameOverSoundDelay);
+          // TODO вывод статистики за игру (функция)
         }
-      } */
+      }
     }
+  }
+
+  isGameLost(): boolean {
+    if (this.errorsCounter === 5) {
+      return true;
+    }
+    return false;
+  }
+
+  finalize() {
+    console.log('In finalize');
+    if (this.isGameLost()) {
+      this.soundService.playAudio('game lost');
+    } else {
+      this.soundService.playAudio('game won');
+    }
+    setTimeout(() => {
+      this.isResultsShown = true;
+    }, this.gameOverSoundDelay);
   }
 
   onCloseDialog(): void {
     this.isGamePaused = true;
     const dialogRef = this.dialog.open(CloseDialogComponent, {
-      width: '350px',
+      width: '350px', // TODO установить ширину настройками
     });
 
     dialogRef.afterClosed().subscribe(() => {
