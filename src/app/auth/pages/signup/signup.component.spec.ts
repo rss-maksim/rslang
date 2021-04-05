80f6c56ca47850fdb7661becc6ea4686b76e41d3
ars6300 <ars6300@live.com>
2021-04-06 02:05:40 +0500

    feat: add tests for signup, start-game

--- src/app/auth/pages/signup/signup.component.spec.ts
+++ src/app/auth/pages/signup/signup.component.spec.ts
@@ -6,12 +6,17 @@ import { RouterTestingModule } from '@angular/router/testing';
 
 import { SignupComponent } from './signup.component';
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { By } from '@angular/platform-browser';
 
 describe('SignupComponent', () => {
   let component: SignupComponent;
   let fixture: ComponentFixture<SignupComponent>;
   let store: MockStore;
   let initialState = {};
+  let usernameInput: HTMLInputElement;
+  let emailInput: HTMLInputElement;
+  let passwordInput: HTMLInputElement;
+  let passwordConfirmInput: HTMLInputElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -27,9 +32,71 @@ describe('SignupComponent', () => {
     fixture = TestBed.createComponent(SignupComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    usernameInput = fixture.debugElement.query(By.css('.signup__form-username')).nativeElement;
+    emailInput = fixture.debugElement.query(By.css('.signup__form-email')).nativeElement;
+    passwordInput = fixture.debugElement.query(By.css('.signup__form-password')).nativeElement;
+    passwordConfirmInput = fixture.debugElement.query(By.css('.signup__form-password-confirm')).nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('empty username should return error message', () => {
+    usernameInput.value = '';
+    usernameInput.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    const errorMessage = fixture.debugElement.query(By.css('.signup__username-error')).nativeElement.innerHTML;
+    expect(errorMessage).toContain('Имя пользователя обязательно');
+  });
+
+  it('empty username should return error message', () => {
+    usernameInput.value = 'a';
+    usernameInput.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    const errorMessage = fixture.debugElement.query(By.css('.signup__username-error')).nativeElement.innerHTML;
+    expect(errorMessage).toContain('Имя должно содержать не менее 3 символов');
+  });
+
+  it('empty email should return error message', () => {
+    emailInput.value = '';
+    emailInput.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    const errorMessage = fixture.debugElement.query(By.css('.signup__email-error')).nativeElement.innerHTML;
+    expect(errorMessage).toContain('обязателен');
+  });
+
+  it('invalid email should return error message', () => {
+    emailInput.value = 'qwerty';
+    emailInput.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    const errorMessage = fixture.debugElement.query(By.css('.signup__email-error')).nativeElement.innerHTML;
+    expect(errorMessage).toContain('Введите валидный email');
+  });
+
+  it('empty password should return error message', () => {
+    passwordInput.value = '';
+    passwordInput.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    const errorMessage = fixture.debugElement.query(By.css('.signup__password-error')).nativeElement.innerHTML;
+    expect(errorMessage).toContain('Пароль обязателен');
+  });
+
+  it('invalid password should return error message', () => {
+    passwordInput.value = 'qwerty';
+    passwordInput.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    const errorMessage = fixture.debugElement.query(By.css('.signup__password-error')).nativeElement.innerHTML;
+    expect(errorMessage).toContain('не менее 8 символов');
+  });
+
+  it('invalid confirmation password should return error message', () => {
+    passwordInput.value = 'Qwerty1!';
+    passwordInput.dispatchEvent(new Event('input'));
+    passwordConfirmInput.value = 'Qwerty1!!';
+    passwordConfirmInput.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    const errorMessage = fixture.debugElement.query(By.css('.signup__password-confirm-error')).nativeElement.innerHTML;
+    expect(errorMessage).toContain('Пароли не совпадают');
   });
 });
--- src/app/mini-games/shared/components/start-game/start-game.component.spec.ts
+++ src/app/mini-games/shared/components/start-game/start-game.component.spec.ts
@@ -23,4 +23,26 @@ describe('StartGameComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should emit round length when chosen', () => {
+    spyOn(component.roundLength, 'emit');
+    component.onSetRoundLength(1);
+    expect(component.roundLength.emit).toHaveBeenCalledWith(1);
+  });
+
+  it('should emit rounds number when chosen', () => {
+    spyOn(component.numOfRounds, 'emit');
+    component.onSetNumOfRounds(1);
+    expect(component.numOfRounds.emit).toHaveBeenCalledWith(1);
+  });
+
+  it('should emit difficulty level when start clicked', () => {
+    spyOn(component.difficultySelected, 'emit');
+    component.difficulty = 1;
+    component.onStart();
+    expect(component.difficultySelected.emit).toHaveBeenCalledWith(1);
+    component.hasDifficultySlider = false; //if chosen not to have difficulty slider
+    component.onStart();
+    expect(component.difficultySelected.emit).toHaveBeenCalledWith(undefined);
+  });
 });
