4d3bf0633276f6398c111af54022f9f391a1579b
Maksim Kirilko <maksimkirilko@gmail.com>
2021-04-08 23:02:22 +0300

    Hide action buttons if user is not authorized

--- src/app/textbook/components/word-card/word-card.component.ts
+++ src/app/textbook/components/word-card/word-card.component.ts
@@ -1,12 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Store } from '@ngrx/store';
+
 import { AppState } from 'src/app/redux/models/state.model';
 import { selectWordSettingsAddButtons, selectWordSettingsTranslation } from 'src/app/redux/selectors/textbook.selector';
-import { MiniGamesHttpService } from 'src/app/services/mini-games-http.service';
-
-import { playRawSound, playRawSoundArr } from '../../../mini-games/sprint/utils/utils';
-import { ApiService } from '../../../core/services/api.service';
 import { IWord } from 'src/app/redux/models/textbook.model';
+import { playRawSoundArr } from '../../../mini-games/sprint/utils/utils';
+import { ApiService } from '../../../core/services/api.service';
 
 @Component({
   selector: 'app-word-card',
@@ -17,6 +16,7 @@ export class WordCardComponent {
   @Input() item: any;
   @Input() correctWordsCount = 0;
   @Input() wrongWordsCount = 0;
+  @Input() isAuthorized: boolean | null = false;
   @Output() updateUserWord = new EventEmitter();
   isTranslation$ = this.store.select(selectWordSettingsTranslation);
   isSettingButtons$ = this.store.select(selectWordSettingsAddButtons);
@@ -24,7 +24,7 @@ export class WordCardComponent {
   constructor(private store: Store<AppState>, public api: ApiService) {}
 
   wordSound(arr: any) {
-    let audioArr: string[] = arr.map((str: string) => `${this.api.githubAssetUrl}/${str}`);
+    const audioArr: string[] = arr.map((str: string) => `${this.api.githubAssetUrl}/${str}`);
     playRawSoundArr(audioArr);
   }
 
