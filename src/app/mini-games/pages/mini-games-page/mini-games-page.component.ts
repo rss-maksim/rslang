4a022394aa5cb073c2e450991d0ef1f0806aeaaf
Ivan Grakov <ivan.grakov@gmail.com>
2021-04-12 19:17:22 +0300

    refactor: add tooltips on mini-games page

--- src/app/mini-games/pages/mini-games-page/mini-games-page.component.ts
+++ src/app/mini-games/pages/mini-games-page/mini-games-page.component.ts
@@ -1,40 +1,15 @@
 import { Component } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
 import { GameCard, GAME_CARDS_NEW } from 'src/app/core/constants/mini-games';
 
 @Component({
   selector: 'app-mini-games-page',
   templateUrl: './mini-games-page.component.html',
   styleUrls: ['./mini-games-page.component.scss'],
-  animations: [
-    trigger('cardFlip', [
-      state('default', style({ transform: 'none' })),
-      state('flipped', style({ transform: 'rotateY(180deg)' })),
-      transition('* => *', animate('400ms')),
-    ]),
-  ],
 })
 export class MiniGamesPageComponent {
   gameCards: GameCard[] = GAME_CARDS_NEW;
-  flip = 'default';
 
   trackByCardName(index: number, card: GameCard): string {
     return card.name;
   }
-
-  onToggleFlip() {
-    this.flip = this.flip === 'default' ? 'flipped' : 'default';
-    console.log(this.flip);
-  }
-
-  /*
-  onFlip() {
-    this.flip = 'flipped';
-    console.log(this.flip);
-  }
-
-  onUnflip() {
-    this.flip = 'default';
-    console.log(this.flip);
-  }*/
 }
