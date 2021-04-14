593e3f2039ea473691cae3bf732a3ff24dba8735
Ivan Grakov <ivan.grakov@gmail.com>
2021-04-14 17:48:30 +0300

    feat: add toggle switch to change themes

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -23,12 +23,14 @@ export class HeaderComponent implements OnInit {
   headerMenu = headerMenu;
   isLoginPage = false;
   themes = [
+    { value: 'blue-teal', viewValue: 'Blue & Teal' },
     { value: 'deeppurple-amber', viewValue: 'Deep Purple & Amber' },
     { value: 'indigo-pink', viewValue: 'Indigo & Pink' },
     { value: 'pink-bluegrey', viewValue: 'Pink & Blue-Grey' },
     { value: 'purple-green', viewValue: 'Purple & Green' },
   ];
-  selectedValue = 'deeppurple-amber';
+  selectedValue = 'blue-teal';
+  isChecked = false;
 
   constructor(private store: Store<AppState>, private router: Router) {}
 
@@ -46,20 +48,38 @@ export class HeaderComponent implements OnInit {
     this.store.dispatch(logoutUser());
   }
 
-  onSelect(selection: any) {
+  onSliderToggle(toggle: any) {
+    console.log(toggle.checked);
+    this.isChecked = toggle.checked;
+    if (this.isChecked) {
+      document.body.classList.remove('blue-teal');
+      document.body.classList.add('pink-bluegrey');
+    } else {
+      document.body.classList.remove('pink-bluegrey');
+      document.body.classList.add('blue-teal');
+    }
+  }
+
+  /*onSelect(selection: any) {
     switch (selection.target.value) {
+      case 'blue-teal': {
+        document.body.classList.remove('deeppurple-amber', 'indigo-pink', 'pink-bluegrey', 'purple-green');
+        document.body.classList.add('blue-teal');
+        break;
+      }
+
       case 'deeppurple-amber': {
-        document.body.classList.remove('indigo-pink', 'pink-bluegrey', 'purple-green');
+        document.body.classList.remove('blue-teal', 'indigo-pink', 'pink-bluegrey', 'purple-green');
         document.body.classList.add('deeppurple-amber');
         break;
       }
       case 'indigo-pink': {
-        document.body.classList.remove('deeppurple-amber', 'pink-bluegrey', 'purple-green');
+        document.body.classList.remove('blue-teal', 'deeppurple-amber', 'pink-bluegrey', 'purple-green');
         document.body.classList.add('indigo-pink');
         break;
       }
       case 'pink-bluegrey': {
-        document.body.classList.remove('deeppurple-amber', 'indigo-pink', 'purple-green');
+        document.body.classList.remove('blue-teal', 'deeppurple-amber', 'indigo-pink', 'purple-green');
         document.body.classList.add('pink-bluegrey');
         break;
       }
@@ -69,5 +89,5 @@ export class HeaderComponent implements OnInit {
         break;
       }
     }
-  }
+  }*/
 }
